572299e4c773b475b4fc8f061fb2eeaa2402427b
zidane <83826369+zidanneee@users.noreply.github.com>
2021-05-14 08:58:38 +0700

    Update floyd-triangle.cpp

--- floyd-triangle.cpp
+++ floyd-triangle.cpp
@@ -2,26 +2,26 @@
 
 int main() 
 {
-    int n;                              // deklarasi integer n, m
+    int n;                              
     int m = 1;
-    scanf("%d", &n);                    // masukin input ke integer n
+    scanf("%d", &n);                    
 
-    for (int i = 0; i < n; i++)         // semisal n=3; i<3(true) maka jalanin perintah; i+1 jika perintah diulang
+    for (int i = 0; i < n; i++)         
     {                                   
-        for (int j = 0; j < i; j++)     // j=0; 0<0 (didalam cpp dianggap true); diulang sampai j<i(false); j+1 jika perintah diulang
+        for (int j = 0; j < i; j++)     
         {
-            printf("%d ", m);           // mengeluarkan output sebesar m=1
-            m++;                        // m = m+1
+            printf("%d ", m);           
+            m++;                        
         }
-        printf("\n");                   // new line 
-    }                                   // jalanin sampe i<n(false)
-    for (int i = n; i > 0; i--)         // i=3; i>0(true); perintah diulang sampai i>0(false)
+        printf("\n");                   
+    }                                   
+    for (int i = n; i > 0; i--)         
     {
-        for (int j = 0; j < i; j++)     // j=0; 0<3(true); perintah diulang sampai j<i(false); j+1 jika perintah diulang
+        for (int j = 0; j < i; j++)     
         {
-            printf("%d ", m);           // mengeluarkan otput sebesar m
-            m++;                        // tambah nilai m
+            printf("%d ", m);           
+            m++;                        
         }
-        printf("\n");                   //new line
+        printf("\n");                   
     }
 }
